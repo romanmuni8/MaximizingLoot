32cd7baf964e09e17d23baf767a6d0a1f06d7f94
Roman Kozulia <rk8318@rit.edu>
2020-12-26 09:25:38 -0500

    All tests passed

--- src/FractionalKnapsack.java
+++ src/FractionalKnapsack.java
@@ -3,10 +3,49 @@ import java.util.Scanner;
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
         double value = 0;
+
+        int i = values.length;
+        int bestLoot;
+        while(capacity>0){
+            bestLoot = BestItem(values, weights);
+            if(bestLoot==-1){
+                return Math.round(value*10000)/10000.0d;
+            }
+            if( weights[bestLoot]<=capacity){
+                value+=values[bestLoot];
+                capacity -=weights[bestLoot];
+                weights[bestLoot] =0;
+                values[bestLoot]= 0;
+
+            }else{
+                double perPound = (double)values[bestLoot]/(double)weights[bestLoot];
+                double newValue = capacity*perPound;
+                value += newValue;
+                capacity=0;
+
+                weights[bestLoot]-=capacity;
+                values [bestLoot]-=newValue;
+            }
+
+        }
         //write your code here
 
-        return value;
+        return Math.round(value*10000)/10000.0d;
     }
+    //this method will return the best item in terms of its value per weight
+    private static int BestItem(int []values, int [] weights ){
+        double maxValuePerWeight = 0;
+        int bestItem = -1;
+        for( int i =0; i<values.length;i ++){
+            if( weights[i]!= 0 && ((double)values[i]/(double)weights[i])>maxValuePerWeight){
+             bestItem = i;
+             maxValuePerWeight = (double)values[i]/(double)weights[i];
+            }
+        }
+
+        return bestItem;
+    }
+
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
